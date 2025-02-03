5c3bcd7aeb246095fcec6dfec6fb4963893b3601
Andreas Zelios <a.zelios@satways.net>
2025-02-03 23:44:30 +0200

    start of excersise 1

---
+++ ex1.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var args = require("minimist")(process.argv.slice(2), {
+    boolean: [help],
+    string: [file]
+});
+
+if (args.help) {
+
+    printHelp();
+} else if (args.file) {
+
+    console.log(args.file);
+} else {
+
+    error('Incorrect usage.', true);
+}
+
+// ************************
+
+function error(msg, includeHelp = false) {
+    console.log(msg);
+    if (includeHelp) {
+        console.log("");
+        printHelp();
+    }
+}
+
+function printHelp() {
+    console.log("ex1 usage:");
+    console.log("");
+    console.log("--help                      print this help");
+}
+
+
